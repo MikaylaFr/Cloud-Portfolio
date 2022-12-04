bf2d2da51163bc904f80677e7b9729e4a8a30154
Mikayla <friendm@oregonstate.edu>
2022-12-03 22:53:14 -0800

    Post and get loads

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const router = module.exports = require('express').Router();
-const users = require('./users')
-const boats = require('./boats')
+const users = require('./users');
+const boats = require('./boats');
+const loads = require('./loads');
 router.use('/boats', boats.router);
-//router.use('/loads', require('./load'));
+router.use('/loads', loads.router);
 router.use('/', users.router);
